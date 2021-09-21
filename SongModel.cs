bbe7cdf47ae7917ba39725d1e94ea15c1ea82608
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-09-21 19:20:48 +0200

    Update SongModel.cs

--- SongModel.cs
+++ SongModel.cs
@@ -1,18 +1,5 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using Newtonsoft.Json;
-using System.Linq;
-using Microsoft.Azure.Cosmos;
-using Microsoft.Azure.Documents.Client;
-using System.ComponentModel;
 
 namespace AzureFunctionCosmosDb
 {
